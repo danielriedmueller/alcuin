c4686b3c0c4c07fc2f19f0dc5364775a6cbe1bd5
danielriedmueller <daniel.riedmuller@gmail.com>
2020-11-05 11:55:14 +0100

    init hook

---
+++ includes/Alcuin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MediaWiki\Extension\Alcuin;
+
+class Alcuin
+{
+    public function init()
+    {
+        global $wgNamespacesToBeSearchedDefault;
+        global $wgHiddenPrefs;
+        global $egChameleonLayoutFile;
+        global $wgShowExceptionDetails;
+        global $wgCacheDirectory;
+        global $wgMainCacheType;
+        global $wgEnableParserCache;
+        global $wgCachePages;
+        global $wgResourceLoaderMaxage;
+        global $wgRestrictDisplayTitle;
+        global $wgSemanticWikibaseLanguage;
+        global $wgGroupPermissions;
+
+
+    }
+}
--- includes/HookHandler.php
+++ includes/HookHandler.php
@@ -7,62 +7,8 @@ use MediaWiki\Hook\BeforePageDisplayHook;
 class HookHandler implements BeforePageDisplayHook {
 
     public static function onExtensionLoad(): void {
-        global $wgNamespacesToBeSearchedDefault;
-        global $wgHiddenPrefs;
-        global $egChameleonLayoutFile;
-        global $wgShowExceptionDetails;
-        global $wgCacheDirectory;
-        global $wgMainCacheType;
-        global $wgEnableParserCache;
-        global $wgCachePages;
-        global $wgResourceLoaderMaxage;
-        global $wgRestrictDisplayTitle;
-        global $wgEnableWikibaseRepo;
-        global $wgEnableWikibaseClient;
-        global $wgLocalDatabases;
-        global $IP;
-        global $wgGroupPermissions;
-        global $wgExtensionFunctions;
-        global $wgSemanticWikibaseLanguage;
-
-        wfLoadExtension( 'SemanticMediaWiki' );
-        wfLoadExtension( 'SemanticWikibase' );
-
-        $wgEnableWikibaseRepo = true;
-        $wgEnableWikibaseClient = false;
-        require_once "$IP/extensions/Wikibase/repo/Wikibase.php";
-        require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
-        $wgWBRepoSettings['siteLinkGroups'] = [ 'mywikigroup' ];
-        $wgLocalDatabases = [ 'enwiki', 'fawiki' ];
-        $wgWBRepoSettings['localClientDatabases'] = array(
-            'en' => 'enwiki',
-            'fa' => 'fawiki'
-        );
-        $wgSemanticWikibaseLanguage = 'en';
-        $wgGroupPermissions['user']['item-term'] = true;
-        $wgGroupPermissions['user']['item-create'] = true;
-
-        $wgNamespacesToBeSearchedDefault = [
-            NS_BAR => true,
-            NS_FOO => true
-        ];
-
-        $wgHiddenPrefs[] = 'skin';
-        $egChameleonLayoutFile= dirname(__DIR__, 1) . '/layouts/standard.xml';
-        $wgShowExceptionDetails = true;
-        $wgCacheDirectory = false;
-        $wgMainCacheType = CACHE_NONE;
-        $wgEnableParserCache = false;
-        $wgCachePages = false;
-        $wgResourceLoaderMaxage = 0;
-        $wgRestrictDisplayTitle = false;
-
-        enableSemantics();
-
-        $wgExtensionFunctions[] = function() {
-            $GLOBALS['wgExtraNamespaces'][SMW_NS_PROPERTY] = 'SemanticProperty';
-            $GLOBALS['wgExtraNamespaces'][SMW_NS_PROPERTY_TALK] = 'SemanticProperty_talk';
-        };
+      $alcuin = new Alcuin();
+      $alcuin->init();
     }
 
     /**
