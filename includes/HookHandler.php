bf52aa42d2e99ea73a55a547dfe750b71d3fbc00
danielriedmueller <daniel.riedmuller@gmail.com>
2020-11-05 13:49:06 +0100

    new pages get ids as titles

--- includes/HookHandler.php
+++ includes/HookHandler.php
@@ -3,6 +3,9 @@
 namespace MediaWiki\Extension\Alcuin;
 
 use MediaWiki\Hook\BeforePageDisplayHook;
+use MediaWiki\MediaWikiServices;
+use Title;
+use WikiPage;
 
 class HookHandler implements BeforePageDisplayHook {
 
@@ -27,5 +30,36 @@ class HookHandler implements BeforePageDisplayHook {
 
     public function onSkinBuildSidebar($skin, &$bar): void {
         Navigation::enrichSidebarNavigation($bar);
+    }
+
+    /**
+     * Occurs after the save page request has been processed.
+     *
+     * @param WikiPage $wikiPage
+     * @param User $user
+     * @param Content $content
+     * @param string $summary
+     * @param bool $isMinor
+     * @param null $isWatch Unused
+     * @param null $section Unused
+     * @param int $flags
+     * @param Revision|null $revision
+     * @param Status $status
+     * @param int|false $originalRevId
+     * @param int $undidRevId
+     *
+     * @return boolean
+     * @see https://www.mediawiki.org/wiki/Manual:Hooks/PageContentSaveComplete
+     */
+    public static function onPageContentSaveComplete(WikiPage $wikiPage, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $revision, $status, $originalRevId, $undidRevId)
+    {
+        $title = $wikiPage->getTitle();
+        $id = $wikiPage->getId();
+        $movePageFactory = MediaWikiServices::getInstance()->getMovePageFactory();
+        $newPage = Title::makeTitle($title->getNamespace(), "Q" . $id);
+        $movePage = $movePageFactory->newMovePage( $title, $newPage );
+        $movePage->move($user);
+
+        return true;
     }
 }
