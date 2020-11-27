d6c0ee8b55c6aaf4d67a9a8f02a9d8d5284ce4da
danielriedmueller <daniel.riedmuller@gmail.com>
2020-11-27 10:01:48 +0100

    stats above nav

--- resources/script.js
+++ resources/script.js
@@ -1,1 +1,24 @@
-console.log("JS works!");
+$(document).ready(function(){
+
+    const pagesCount = $('<div></div>').css({"font-size": "0.75em"});
+    const jobsCount = $('<div></div>').css({"font-size": "0.75em"});
+    const infoEl = $('<div></div>')
+        .css({"margin": "0 0.6em 0 1.2em"})
+        .append(pagesCount)
+        .append(jobsCount)
+    ;
+
+    mw.loader.using( 'mediawiki.api', function () {
+        (new mw.Api()).get( {
+            action: 'query',
+            meta: 'siteinfo',
+            siprop: 'statistics'
+        }).done( function ( data ) {
+            const stats = data.query.statistics;
+            pagesCount.text('Seiten: ' + stats.pages);
+            jobsCount.text('Jobs: ' + stats.jobs);
+
+            $('#p-navigation').before(infoEl)
+        });
+    });
+});
