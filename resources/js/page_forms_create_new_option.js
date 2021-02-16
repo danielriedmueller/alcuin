297861b145d61dbe16161d8d876250a94862962d
danielriedmueller <daniel.riedmuller@gmail.com>
2021-02-16 11:09:09 +0100

    js page forms create new option optimized

--- resources/js/page_forms_create_new_option.js
+++ resources/js/page_forms_create_new_option.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    const selectEl = $("select[createnew]");
-    if (selectEl.length > 0) {
-        const category = selectEl.attr('createnew');
-        const addBtn = $('<a>');
+    var selectEl = $("select[createnew]");
+    selectEl.each(function(el) {
+        var category = el.attr('createnew');
+        var addBtn = $('<a>');
         addBtn
             .attr('href', `/index.php/Form:${category}`)
             .attr('target', `_blank`)
             .addClass('button')
             .text('Neu');
-        selectEl.after(addBtn);
-    }
+        el.after(addBtn);
+    })
 });
