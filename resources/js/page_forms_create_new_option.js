da98028fd4af7fa02bed33d64335e25e7077c1e3
danielriedmueller <daniel.riedmuller@gmail.com>
2021-02-16 11:13:14 +0100

    js fix

--- resources/js/page_forms_create_new_option.js
+++ resources/js/page_forms_create_new_option.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var selectEl = $("select[createnew]");
-    selectEl.each(function(el) {
-        var category = el.attr('createnew');
+    selectEl.each(function(index, el) {
+        var category = $(el).attr('createnew');
         var addBtn = $('<a>');
         addBtn
             .attr('href', `/index.php/Form:${category}`)
             .attr('target', `_blank`)
             .addClass('button')
             .text('Neu');
-        el.after(addBtn);
+        $(el).after(addBtn);
     })
 });
