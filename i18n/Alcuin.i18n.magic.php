a5f51b8f6b2e12898c27c641b6d6cace5e968e78
danielriedmueller <daniel.riedmuller@gmail.com>
2021-03-18 10:47:15 +0100

    sparql pagename magic word

---
+++ i18n/Alcuin.i18n.magic.php
@@ -0,0 +1,11 @@
+<?php
+
+$magicWords = [];
+
+$magicWords['en'] = [
+    'sparqlpagename' => [ 0, 'sparqlpagename' ],
+];
+
+$magicWords['de'] = [
+    'sparqlpagename' => [ 0, 'sparqlseitenname', 'sparqlpagename' ],
+];
