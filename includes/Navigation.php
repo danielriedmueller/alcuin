a6f9b1a52a0eb671d5e1b17d10019a8c5815ec41
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-11-15 14:43:15 +0100

    navigation zu formularen

--- includes/Navigation.php
+++ includes/Navigation.php
@@ -4,58 +4,68 @@ namespace MediaWiki\Extension\Alcuin;
 
 class Navigation
 {
-    public static function enrichSidebarNavigation(&$bar) {
-        $bar['navigation'] = [[
-            'text'   => 'Bearbeiten',
-            'href'   => '/index.php/Alcuin:Bearbeiten',
-            'id'     => 'n-edit-form-chooser',
-            'active' => ''
-        ],[
-            'text'   => 'Alle Seiten',
-            'href'   => '/index.php/Special:AllPages',
-            'id'     => 'n-pages',
-            'active' => ''
-        ], [
-            'text'   => 'Alle Kategorien',
-            'href'   => '/index.php/Special:Categories',
-            'id'     => 'n-categories',
-            'active' => ''
-        ], [
-            'text'   => 'Alle Attribute',
-            'href'   => '/index.php/Special:Properties',
-            'id'     => 'n-attributes',
-            'active' => ''
-        ], [
-            'text'   => 'Alle Vorlagen',
-            'href'   => '/index.php/Special:Templates',
-            'id'     => 'n-templates',
-            'active' => ''
-        ], [
-            'text'   => 'Alle Formulare',
-            'href'   => '/index.php/Special:Forms',
-            'id'     => 'n-forms',
-            'active' => ''
-        ]];
-        $bar['erstellen'] = [[
-            'text'   => 'Vorlage',
-            'href'   => '/index.php/Special:CreateTemplate',
-            'id'     => 'n-create-template',
-            'active' => ''
-        ], [
-            'text'   => 'Formular',
-            'href'   => '/index.php/Special:CreateForm',
-            'id'     => 'n-create-form',
-            'active' => ''
-        ], [
-            'text'   => 'Klasse',
-            'href'   => '/index.php/Special:CreateClass',
-            'id'     => 'n-create-class',
-            'active' => ''
-        ], [
-            'text'   => 'Attribut',
-            'href'   => '/index.php/Special:CreateProperty',
-            'id'     => 'n-create-attribute',
-            'active' => ''
-        ]];
+    public static function enrichSidebarNavigation(&$bar)
+    {
+        $bar['navigation'] = [
+            [
+                'text' => 'Alle Seiten',
+                'href' => '/index.php/Special:AllPages',
+                'id' => 'n-pages',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Kategorien',
+                'href' => '/index.php/Special:Categories',
+                'id' => 'n-categories',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Attribute',
+                'href' => '/index.php/Special:Properties',
+                'id' => 'n-attributes',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Vorlagen',
+                'href' => '/index.php/Special:Templates',
+                'id' => 'n-templates',
+                'active' => '',
+            ],
+        ];
+
+        //Create Form links
+        $forms = [
+            'Author' => 'Autor',
+            'Book' => 'Buch',
+            'Collection' => 'Sammelband',
+            'Edition' => 'Edierter Text',
+            'Editor' => 'Editor',
+            'InCollection' => 'Sammbelbandbeitrag',
+            'Incunable' => 'Inkunabel',
+            'Journal' => 'Zeitschrift',
+            'JournalArticle' => 'Zeitschriftenaufsatz',
+            'Library' => 'Bibliothek',
+            'LiteratureReference' => 'Literaturverweis',
+            'Manuscript' => 'Manuskript',
+            'ManuscriptReference' => 'Manuskriptverweis',
+            'OperaOmnia' => 'Opera Omnia',
+            'Order' => 'Orden',
+            'Print' => 'Druck',
+            'Printer' => 'Drucker',
+            'Publisher' => 'Herausgeber',
+            'Series' => 'Reihe',
+            'Subseries' => 'Unterreihe',
+            'Work' => 'Werk',
+            'WorkPart' => 'Werkteil',
+        ];
+
+        foreach ($forms as $form => $name) {
+            $bar['Formulare'][] = [
+                'text' => $name,
+                'href' => '/index.php/Spezial:Mit_Formular_bearbeiten/' . $form,
+                'target' => 'blank',
+                'active' => '',
+            ];
+        }
     }
 }
