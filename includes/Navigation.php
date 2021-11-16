ca267c54b9f6e35ca0cb6c79916a56a3ab4f5351
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-11-16 10:09:24 +0100

    navigation update

--- includes/Navigation.php
+++ includes/Navigation.php
@@ -6,33 +6,6 @@ class Navigation
 {
     public static function enrichSidebarNavigation(&$bar)
     {
-        $bar['navigation'] = [
-            [
-                'text' => 'Alle Seiten',
-                'href' => '/index.php/Special:AllPages',
-                'id' => 'n-pages',
-                'active' => '',
-            ],
-            [
-                'text' => 'Alle Kategorien',
-                'href' => '/index.php/Special:Categories',
-                'id' => 'n-categories',
-                'active' => '',
-            ],
-            [
-                'text' => 'Alle Attribute',
-                'href' => '/index.php/Special:Properties',
-                'id' => 'n-attributes',
-                'active' => '',
-            ],
-            [
-                'text' => 'Alle Vorlagen',
-                'href' => '/index.php/Special:Templates',
-                'id' => 'n-templates',
-                'active' => '',
-            ],
-        ];
-
         //Create Form links
         $forms = [
             'Author' => 'Autor',
@@ -67,5 +40,32 @@ class Navigation
                 'active' => '',
             ];
         }
+
+        $bar['Durchsuchen'] = [
+            [
+                'text' => 'Alle Seiten',
+                'href' => '/index.php/Special:AllPages',
+                'id' => 'n-pages',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Kategorien',
+                'href' => '/index.php/Special:Categories',
+                'id' => 'n-categories',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Attribute',
+                'href' => '/index.php/Special:Properties',
+                'id' => 'n-attributes',
+                'active' => '',
+            ],
+            [
+                'text' => 'Alle Vorlagen',
+                'href' => '/index.php/Special:Templates',
+                'id' => 'n-templates',
+                'active' => '',
+            ],
+        ];
     }
 }
