f8994b80ab12e9a267942512afa19bd05b3495f6
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-12-17 11:40:06 +0100

    suppress leaving page alert

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,5 @@
+window.onbeforeunload = null;
 $(document).on("keydown", "form#pfForm :input:not(textarea)", function(event) {
     return event.key != "Enter";
 });
-window.onbeforeunload = null;
+
