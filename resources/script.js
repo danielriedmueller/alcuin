f770da640167f7fd0a6bd783ebdfa97013e7717c
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-12-17 11:39:15 +0100

    style

--- resources/script.js
+++ resources/script.js
@@ -1,6 +1,4 @@
-$(document).ready(() => {
-
-});
 $(document).on("keydown", "form#pfForm :input:not(textarea)", function(event) {
     return event.key != "Enter";
 });
+window.onbeforeunload = null;
