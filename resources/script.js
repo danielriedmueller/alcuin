af946bb9c87c78bcc54af9cb31cad5e96d5c2fa3
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-12-22 12:21:59 +0100

    style

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,16 @@
 $('#pageId').click(function(e) {
-    console.log(e);
-    console.log($(e.currentTarget).text());
-    navigator.clipboard.writeText($(e.currentTarget).text());
+    var text = $(e.currentTarget).text();
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text);
+    } else {
+        if (window.clipboardData) { // Internet Explorer
+            window.clipboardData.setData("Text", text);
+        } else {
+            unsafeWindow.netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+            const clipboardHelper = Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper);
+            clipboardHelper.copyString(text);
+        }
+    }
 });
 $(document).on("keydown", "form#pfForm :input:not(textarea)", function(event) {
     return event.key != "Enter";
