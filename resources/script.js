73d559e3fa373667ccc80b03be2e6680e7ced18b
Daniel Riedmüller <daniel.riedmueller@iserv.eu>
2021-12-22 12:10:32 +0100

    click to copy id

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,6 @@
+$('#pageId').click(function(e) {
+    navigator.clipboard.writeText($(e.currentTarget).text());
+});
 $(document).on("keydown", "form#pfForm :input:not(textarea)", function(event) {
     return event.key != "Enter";
 });
